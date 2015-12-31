85847747bae96accc836b4ec12ebe75386825ca8
saviorZSC <savior_zsc@163.com>
2015-12-31 13:06:35 +0800

    this is update 2015-12-31

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,10 +19,50 @@ $factory->define(App\User::class, function ($faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+//post表的填充数据
+/*$factory->define(App\Models\Post::class, function ($faker) {
+    return [
+        'title' => $faker->sentence(mt_rand(3, 10)),
+        'content' => join("\n\n", $faker->paragraphs(mt_rand(3, 6))),
+        'published_at' => $faker->dateTimeBetween('-1 month', '+3 days'),
+    ];
+});
+
+
+
 $factory->define(App\Models\Article::class,function($faker){
     return [
         'title' => $faker->sentence(mt_rand(3,10)),
         'content' => join('\n\n',$faker->paragraphs(mt_rand(3,6))),
         'publish_time' => $faker->dateTimeBetween('-1 month', '+3 days'),
     ];
+});*/
+
+$factory->define(App\Models\Post::class, function ($faker) {
+    $images = ['about-bg.jpg', 'contact-bg.jpg', 'home-bg.jpg', 'post-bg.jpg'];
+    $title = $faker->sentence(mt_rand(3, 10));
+    return [
+        'title' => $title,
+        'subtitle' => str_limit($faker->sentence(mt_rand(10, 20)), 252),
+        'page_image' => $images[mt_rand(0, 3)],
+        'content_raw' => join("\n\n", $faker->paragraphs(mt_rand(3, 6))),
+        'published_at' => $faker->dateTimeBetween('-1 month', '+3 days'),
+        'meta_description' => "Meta for $title",
+        'is_draft' => false,
+    ];
 });
+
+$factory->define(App\Models\Tag::class, function ($faker) {
+    $images = ['about-bg.jpg', 'contact-bg.jpg', 'home-bg.jpg', 'post-bg.jpg'];
+    $word = $faker->word;
+    return [
+        'tag' => $word,
+        'title' => ucfirst($word),
+        'subtitle' => $faker->sentence,
+        'page_image' => $images[mt_rand(0, 3)],
+        'meta_description' => "Meta for $word",
+        'reverse_direction' => false,
+    ];
+});
+
