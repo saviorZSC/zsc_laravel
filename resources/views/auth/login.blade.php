23460a27b06aabf62c86bcaf827c9366048f353c
saviorZSC <savior_zsc@163.com>
2015-12-31 13:15:57 +0800

    this is update 2015-12-31

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,31 +6,45 @@
     @include('nweaver.nav')
     <div class="first-section">
         <div class="container">
-            <div class="col-md-6">
-                <form action="/auth/login" method="post">
-                    {!!csrf_field()!!}
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input name="email" type="text" placeholder="email" value="{{old('name')}}" />
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password:</label>
-                        <input name="password" type="password" placeholder="password" />
-                    </div>
-                    <div>
-                        <input type="checkbox" name="remenber" />Remenber Me
-                    </div>
-                    <button type="submit" class="btn btn-primary">Register</button>
-                </form>
-                @if (count($errors) > 0)
-                    <div class="alert alert-danger">
-                        <ul style="color:red;">
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
+            <div class="row">
+                <div class="col-md-12 column">
+                    <form class="form-horizontal" role="form"  action="{{url('/auth/login')}}" method="post">
+                        {!!csrf_field()!!}
+                        <div class="form-group">
+                            <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" value="{{old('email')}}" name="email" class="form-control" id="inputEmail" />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputPassword" class="col-sm-2 control-label">Password</label>
+                            <div class="col-sm-10">
+                                <input type="password" name="password" class="form-control" id="inputPassword" />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <div class="checkbox">
+                                    <label><input type="checkbox" name="remenber" />Remember me</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-primary form-control">Login</button>
+                            </div>
+                        </div>
+                    </form>
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <ul style="color:red;">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                </div>
             </div>
         </div>
     </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,26 +6,38 @@
     @include('nweaver.nav')
     <div class="first-section">
         <div class="container">
-            <div class="col-md-6">
-                <form action="/auth/register" method="POST">
+            <div class="col-md-12">
+                <form class="form-horizontal" role="form"  action="{{url('/auth/register')}}" method="POST">
                     {!!csrf_field()!!}
                     <div class="form-group">
-                        <label for="name">Name:</label>
-                        <input name="name" type="text" placeholder="name" value="{{old('name')}}" />
+                        <label for="name" class="col-sm-2 control-label">Name:</label>
+                        <div class="col-sm-10">
+                            <input name="name" type="text" class="form-control" placeholder="name" value="{{old('name')}}" />
+                        </div>
                     </div>
                     <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input name="email" type="text" placeholder="email" value="{{old('name')}}" />
+                        <label for="email" class="col-sm-2 control-label">Email:</label>
+                        <div class="col-sm-10">
+                            <input name="email" type="text" class="form-control" placeholder="email" value="{{old('email')}}" />
+                        </div>
                     </div>
                     <div class="form-group">
-                        <label for="password">Password:</label>
-                        <input name="password" type="password" placeholder="password" />
+                        <label for="password" class="col-sm-2 control-label">Password:</label>
+                        <div class="col-sm-10">
+                            <input name="password" class="form-control" type="password" placeholder="password" />
+                        </div>
                     </div>
                     <div class="form-group">
-                        <label for="password"_confirmation>Confirm_Password:</label>
-                        <input name="password_confirmation" type="password" placeholder="password" />
+                        <label class="col-sm-2 control-label" for="password_confirmation">Confirm_Password:</label>
+                        <div class="col-sm-10">
+                            <input name="password_confirmation" class="form-control" type="password" placeholder="password" />
+                        </div>
                     </div>
-                    <button type="submit" class="btn btn-primary">Register</button>
+                    <div class="form-group">
+                        <div class="col-sm-6 col-sm-offset-3">
+                            <button type="submit" class="btn btn-primary form-control">Register</button>
+                        </div>
+                    </div>
                 </form>
                 @if (count($errors) > 0)
                     <div class="alert alert-danger">
